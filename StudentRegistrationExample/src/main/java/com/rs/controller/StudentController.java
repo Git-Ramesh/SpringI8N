8f293266b1858f9f319f01b59fb66a9b801e81fa
Ramesh; <rameshpanthangi08@gmail.com>
2018-02-21 17:25:22 +0530

    Exception Handling Functionality added

--- StudentRegistrationExample/src/main/java/com/rs/controller/StudentController.java
+++ StudentRegistrationExample/src/main/java/com/rs/controller/StudentController.java
@@ -1,21 +1,24 @@
 package com.rs.controller;
 
-import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.rs.command.Student;
 import com.rs.editor.StudentNameEditor;
@@ -40,7 +43,9 @@ public class StudentController {
 		return "register";
 	}
 	@RequestMapping(value="/registerSuccess",method=RequestMethod.POST)
-	public String loginSuccess(Map<String,Object> map,@Valid@ModelAttribute("student")Student student,BindingResult results ) {
+	public String loginSuccess(Map<String,Object> map,@Valid @ModelAttribute("student")Student student,BindingResult results ) {
+		if(student.getStudentName().equals("RAMESH"))
+			throw new RuntimeException("Programmer Exception to test error page");
 		if(results.hasErrors())
 			return "register";
 		System.out.println(student);
@@ -84,4 +89,14 @@ public class StudentController {
 		
 		return citiesList;
 	}
+	//Specific to controller
+	@ExceptionHandler(RuntimeException.class)
+	  public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+
+	    ModelAndView mav = new ModelAndView();
+	    mav.addObject("exception", ex);
+	    mav.addObject("url", req.getRequestURL());
+	    mav.setViewName("runtime_error");
+	    return mav;
+	  }
 }
--- StudentRegistrationExample/src/main/webapp/WEB-INF/pages/register_success.jsp
+++ StudentRegistrationExample/src/main/webapp/WEB-INF/pages/register_success.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1" isELIgnored="false"%>
  <%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %> 
+ <%@taglib uri="http://www.springframework.org/tags"  prefix="spring"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,34 +9,34 @@
 <title>Student Details</title>
 </head>
 <body>
-	<h1 style="color:black;text-align:center;">Student Details</h1>
+	<h1 style="color:black;text-align:center;"><spring:message code="header.result_success"/></h1>
 	<table align="center" border="1">
 		<tr>
-			<td>StudentName: </td>
+			<td><spring:message code="label.studentName"/>: </td>
 			<td>${student.studentName}</td>
 		</tr>
 
 		<tr>
-			<td>Gender: </td>
+			<td><spring:message code="label.gender"/>: </td>
 			<td>${student.gender}</td>
 		</tr>
 
 		<tr>
-			<td>Technologies: </td>
+			<td><spring:message code="label.technologies"/>: </td>
 			<td>${student.technologies}</td>
 		</tr>
 
 		<tr>
-			<td>City: </td>
+			<td><spring:message code="label.city"/>: </td>
 			<td>${student.city}</td>
 		</tr>
 		
 		<tr>
-			<td>Phone: </td>
+			<td><spring:message code="label.phone"/> </td>
 			<td>${student.phone}</td>
 		</tr>
 		<tr>
-			<td>DOB</td>
+			<td><spring:message code="label.dob"/></td>
 			<td><fmt:formatDate pattern="MMM dd,yyyy" value="${student.dob}"/> </td>
 		</tr>
 		
